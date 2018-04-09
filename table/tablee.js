3b3577c693fc7429c945db781c3572eb0e9d2291
Duc Trung <ductrung.731990@gmail.com>
2018-04-09 20:23:14 +0700

    up
    1

---
+++ table/tablee.js
@@ -0,0 +1,234 @@
+var sp1 = {
+	STT: "1",
+	Tenthuoc:"Panadol Extra",
+	Mau: "image/panadol.jpeg",
+	ThanhPhan: "500 mg Paracetamol, 65 mg Caffeine",
+	CongDung: "Điều trị đau nhẹ đến vừa và hạ sốt",
+	QuyCach: "Hộp 30 vỉ x 4 viên",
+	Gia: "500.000 vnd",
+}
+
+var sp2 = {
+	STT: "2",
+	Tenthuoc:"Boni Ancol",
+	Mau: "image/boniancol.jpeg",
+	ThanhPhan: "Vitamine B6: 10mg, L- Glutamine, Magnesium: 15mg, Kava Root: 10mg, N-Acetyl Cystein: 300mg",
+	CongDung: "Giúp làm giảm cảm giác thèm rượu và các thói quen gây nghiện khác. Giúp làm giảm các triệu chứng khó chịu trong quá trình cai rượu. Giải rượu và bảo vệ gan, thận khỏi tác hại của rượu.",
+	QuyCach: "Lọ 60 viên",
+	Gia: "230.000 vnd",
+}
+
+var sp3 = {
+	STT: "3",
+	Tenthuoc:"Boni Diabet",
+	Mau: "image/bonidiabet.jpeg",
+	ThanhPhan: "Magie, kẽm, acid folic, vitamin C, Alpha lipoic acid. Bitter Melon ( Momordica charantia – mướp đắng), gymnema sylvestre ( dây thìa canh).",
+	CongDung: "Hỗ trợ điều trị tiểu đường, làm giảm glucose máu. Ngăn ngừa các biến chứng của bệnh tiểu đường trên tim, gan, thận, võng mạc, các bệnh lý thần kinh ngoại biên, làm lành vết thương, ngăn ngừa xơ vữa động mạch, đục thủy tinh thể. Giúp giảm cholesterol và lipid máu.",
+	QuyCach: "Lọ 60 viên",
+	Gia: "230.000 vnd",
+}
+
+var sp4 = {
+	STT: "4",
+	Tenthuoc:"Boni Smok",
+	Mau: "image/bonismok.jpg",
+	ThanhPhan: "Bồ công anh, kim ngân hoa, cúc hoa, dầu quế ...",
+	CongDung: "Khử mùi hôi miệng do bị viêm lợi, sâu răng hoặc hút thuốc lá, cai thuốc lá",
+	QuyCach: "Chai 250 ml",
+	Gia: "50.000 vnd",
+}
+
+var sp5 = {
+	STT: "5",
+	Tenthuoc:"Trisomos",
+	Mau: "image/trisomos.jpg",
+	ThanhPhan: "Cao chiết dược liệu: Hoa hòe 69mg, Đương quy 55mg, Diếp cá 83mg, Thảo quyết minh 138mg, Cam thảo 55mg",
+	CongDung: "Hỗ trợ điều trị và giúp phòng ngừa, cải thiện các triệu chứng của bệnh trĩ, Hỗ trợ điều trị và phòng ngừa táo bón, Giúp tăng cường tiêu hóa",
+	QuyCach: "Lọ 50 viên",
+	Gia: "150.000 vnd",
+}
+
+var sp6 = {
+	STT: "6",
+	Tenthuoc:"Bổ Phế Nam Hà",
+	Mau: "image/bophenamha.jpg",
+	ThanhPhan: "Bạch Linh, Cát cánh, Mơ muối, Bạch phàn, Cam Thảo, Tỳ bà diệp, Tang bạch bì, Ma Hoàng, Bạc hà diệp, Đường kính, và các tá dược khác vừa đủ.",
+	CongDung: "Sát trùng họng, bổ phổi tiêu đờm… Được sử dụng để điều trị các chứng ho do thay đổi thời tiết, ho cảm, ho gió, ho khan, và các chứng ho do mắc bệnh hô hấp…",
+	QuyCach: "Chai 125ml",
+	Gia: "28.000 vnd",
+}
+
+var sp7 = {
+	STT: "7",
+	Tenthuoc:"Hoạt Huyết Dưỡng Não",
+	Mau: "image/hoathuyet.png",
+	ThanhPhan: "Cao Đinh lăng 150 mg, cao Bạch quả 5 mg.",
+	CongDung: "Bổ khí huyết, hoạt huyết dưỡng não.",
+	QuyCach: "Hộp 2 vỉ x 50 viên",
+	Gia: "20.000 vnd",
+}
+
+var sp8 = {
+	STT: "8",
+	Tenthuoc:"Alpha chymotrypsin®",
+	Mau: "image/alpha.jpg",
+	ThanhPhan: "Chymotrypsin",
+	CongDung: "Kháng Viêm",
+	QuyCach: "Hộp 2 vỉ x 10 viên nén",
+	Gia: "12.000 vnd",
+}
+
+var sp9 = {
+	STT: "9",
+	Tenthuoc:"Gót Sen",
+	Mau: "image/gotsen.jpg",
+	ThanhPhan: "Kết hợp tinh chất Curcumin và Vitamin E",
+	CongDung: "Giúp tái tạo tế bào da mới, giữ ẩm và làm tăng sự đàn hồi cho da",
+	QuyCach: "Tuýp 20g",
+	Gia: "24.000 vnd",
+}
+
+var sp10 = {
+	STT: "10",
+	Tenthuoc:"Dầu Gội Dược Liệu Thái Dương 3",
+	Mau: "image/daugoi.jpg",
+	ThanhPhan: "Tang bạch bì, Bồ kết, Cây ngũ sắc: Các tinh dầu Cadinen, caryophyllen, curmarin, Cỏ mần trầu",
+	CongDung: "Sạch gầu ngay sau khi gội lần đầu tiên Suôn mềm, không cần dùng thêm dầu xả, Thoáng mát da đầu, Chống rụng tóc, mọc lại tóc đặc biết hiệu quả cả với người bị hói",
+	QuyCach: "Chai 200ml",
+	Gia: "40.000 vnd",
+}
+
+var arr = [sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10]
+
+for (var i = 0; i < arr.length; i++) {
+	$("tbody").append("<tr>" +
+				"<td class='col0'>" + arr[i].STT + "</td>" +
+				"<td class='col1'>" + arr[i].Tenthuoc + "</td>" +	
+				"<td class='col2'>" + "<img src = " + arr[i].Mau + ">" + "</td>" +
+				"<td class='col3'>" + arr[i].ThanhPhan + "</td>" +
+				"<td class='col4'>" + arr[i].CongDung + "</td>" +
+				"<td class='col5'>" + arr[i].QuyCach + "</td>" +
+				"<td class='col6'>" + arr[i].Gia + "</td>" +
+			"</tr>");
+}
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable2");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc"; 
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++; 
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+function sortTableNumber(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable2");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc"; 
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = parseInt(rows[i].getElementsByTagName("TD")[n].innerHTML);
+      y = parseInt(rows[i + 1].getElementsByTagName("TD")[n].innerHTML);
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x > y) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x < y) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++; 
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
